6f0f225dc2fc8e3d9f29181b6f0149dfcb572d71
Gustavo Rene <87093351+Gustavin23@users.noreply.github.com>
2022-05-16 16:48:37 -0300

    Add files via upload

---
+++ agenda.sql
@@ -0,0 +1,19 @@
+/**
+	Agenda de contatos
+    @author Gustavo Rene 
+*/
+
+-- Verificar bancos de dados
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendagustavo;
+
+-- Excluir um banco de dados
+drop database teste;
+
+-- Selecionar o banco de dados
+use agendagustavo;
+
+
